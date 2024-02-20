cb9591b6a2cfe423b402e0691d9d360723bb3c85
Surov Vladimir <vovkasu@gmail.com>
2024-02-20 18:38:50 +0200

    Added optional dependence on AppsFlyer

--- Assets/PackageSrc/Runtime/DeepLinksManager.cs
+++ Assets/PackageSrc/Runtime/DeepLinksManager.cs
@@ -44,5 +44,29 @@ namespace UAppToolkit.DeepLinkCommands
                 Debug.LogError($"Incorrect deepLink URL {absoluteUrl}. No DeepLink Command found.");
             }
         }
+
+#if APPS_FLYER_ENABLED
+        public void onConversionDataSuccess(string conversionData)
+        {
+            AppsFlyerSDK.AppsFlyer.AFLog("onConversionDataSuccess", conversionData);
+            var conversionDataDictionary = AppsFlyerSDK.AppsFlyer.CallbackStringToDictionary(conversionData);
+            Debug.Log($"%%%% onConversionDataSuccess {conversionData}");
+            ApplyActions(attributions: conversionDataDictionary);
+        }
+
+        public void onConversionDataFail(string error)
+        {
+            AppsFlyerSDK.AppsFlyer.AFLog("onConversionDataFail", error);
+        }
+
+        public void onAppOpenAttribution(string attributionData)
+        {
+            AppsFlyerSDK.AppsFlyer.AFLog("onAppOpenAttribution", attributionData);
+            var attributionDataDictionary = AppsFlyerSDK.AppsFlyer.CallbackStringToDictionary(attributionData);
+            Debug.Log($"%%%% onAppOpenAttribution {attributionData}");
+
+            ApplyActions(attributions: attributionDataDictionary);
+        }
+#endif
     }
 }
